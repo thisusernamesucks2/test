9dd7ca37917094ced29997db4ab43f2c9d8f2272
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-07 08:57:18 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+const xhr = new XMLHttpRequest();
+xhr.open("POST", "https://cs-api.pltw.org/question?text=test");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = xhr.response;
+    console.log(data);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
