88a00f4c12b440fdbbf32836cbe78a5adb476d98
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-07 09:06:05 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
-const xhr = new XMLHttpRequest();
-xhr.open("POST", "https://cs-api.pltw.org/question?text=test");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    const data = xhr.response;
-    console.log(data);
-  } else {
-    console.log(`Error: ${xhr.status}`);
-  }
-};
+const textbox = document.getElementById('myTextbox');
+const button = document.getElementById('myButton');
+button.addEventListener('click', () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://cs-api.pltw.org/question?text=test");
+  xhr.send();
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const data = xhr.response;
+      console.log(data);
+    } else {
+      console.log(`Error: ${xhr.status}`);
+    }
+  };
+});
