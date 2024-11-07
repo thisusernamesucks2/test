ccd2925f68bc4f380eee8d1884085d70b247a33e
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-07 12:24:36 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,17 +2,15 @@ const textbox = document.getElementById('myTextbox');
 const button = document.getElementById('myButton');
 button.addEventListener('click', () => {
   const xhr = new XMLHttpRequest();
-  if (textbox.value === "%test.runchatservice/8873%.commandline" {
-    xhr.open("POST", "https://cs-api.pltw.org/question?text=test");
-    xhr.send();
-    xhr.responseType = "json";
-    xhr.onload = () => {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        const data = xhr.response;
-        console.log(data);
-      } else {
-        console.log(`Error: ${xhr.status}`);
-      }
+  xhr.open("POST", "https://cs-api.pltw.org/question?text=test");
+  xhr.send();
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const data = xhr.response;
+      console.log(data);
+    } else {
+      console.log(`Error: ${xhr.status}`);
     }
   };
 });
