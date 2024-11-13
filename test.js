e7f9ee44a2a6d657c6a2ae2ec3e44b7b8ddb4291
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-13 09:38:36 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // URL to fetch data from
-const url = "https://cors-anywhere.herokuapp.com/https://cs-api.pltw.org/rant"; // Example API
+const url = "https://thingproxy.freeboard.io/fetch/https://cs-api.pltw.org/rant"; // Example API
 
 // Function to fetch and display data
 async function fetchData() {
